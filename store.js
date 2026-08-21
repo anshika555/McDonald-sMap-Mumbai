b47a65fd30962cf0ea97a1d51b5ce27b2c74789e
anshika555 <anshikasharma1832@gmail.com>
2026-08-21 21:14:26 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,362 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84079647649914,19.080732915073764]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "G41, Santacruz Railway Station Rd, near sheetal jewellers, Bajaj Wadi, Willingdon, Santacruz (West), Mumbai, Maharashtra 400054",
+          "phone": "07400292866"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates":[72.83495998979818,19.067753998097402]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Shop No 532, Kadamba Kudpi House, Linking Rd, Khar, Khar West, Mumbai, Maharashtra 400052",
+          "phone": "08928304001"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.8284626913066,19.05594409576761]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "1st Floor, Shop No, 101, Hill Rd, next to ST. STANISLAUS HIGH SCHOOL, Bandra West, Mumbai, Maharashtra 400050",
+          "phone": "08928304215"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84038997649918,19.101182457784805]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Ground Floor, Liberty Lodge, Dashrathlal Joshi Rd, LIC Colony, Suresh Colony, Vile Parle West, Mumbai, Maharashtra 400056",
+          "phone": "08928212004"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.86623783946163,19.09609946066542]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Domestic Terminal 1B, Chhatrapati Shivaji Maharaj International Airport Mumbai, SHA, Mezzanine Level, near Vile Parle East, Navpada, Sahar Village, Vile Parle, Mumbai, Maharashtra 400099",
+          "phone": "09372119011"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.86809151505855,19.075077829449867]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Unit No 11, Sumit Artista, near Central Plaza, Kolivery Village, Kunchi Kurve Nagar, Kalina, Santacruz East, Mumbai, Maharashtra 400098",
+          "phone": "08928304299"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.8558919452157,19.11939259868248]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Western Express Highway Metro Station, Unpaid area concourse -1 near Andheri Kurla road, flyover, next to Andheri, Andheri East, Mumbai, Maharashtra 400093",
+          "phone": "09372119028"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.87425971218642,19.100739442683828]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "CSIA, Level 4, International Departures Mumbai, Chatrapati Shivaji International Airport, Hardcastle Restaurants Pvt Ltd, McDonalds Family Restaurants, Mumbai-Airport T2-Intl Depart,Terminal 2, Mumbai, Maharashtra 400099",
+          "phone": "08591202376"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84593558554934,19.122798599936072]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "No 18, Unity Mansion, Gazdar Road, opposite Andheri Station, Railway Colony, Andheri West, Mumbai, Maharashtra 400058",
+          "phone": "08928304008"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.88867926756528,19.08759987031447]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "S1, S2, Phoenix Market City Mall, Food Court, Lal Bahadur Shastri Marg, Ashok Nagar, Kurla, Mumbai, Maharashtra 400070",
+          "phone": "08928304142"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.86396002977294,19.047688112248498]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "SN 09, Plot No 1A, Basant Court CHS Ltd Sion, opposite Sion Railway, Station, Mumbai, Maharashtra 400022",
+          "phone": "08928304285"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84748053791137,19.120852322100856]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Hingu Bhavan Hingu Bhavan Andheri East Station, Sahar Rd, near Agarkar Chowk, Mumbai, Maharashtra 400069",
+          "phone": "08928210222"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.88078284438161,19.06942985503866]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "East Point Mall, Shop No.02 Ground Floor, Shop No.2 First Floor, SG Barve Marg, Nehru Nagar, Kurla, Mumbai, Maharashtra 400024",
+          "phone": "07021359455"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.88026786026094,19.099279541713315]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Terminal 2, CSIA Level 3, Domestic Departure SHA Mumbai Chatrapati Shivaji International Airport, Sahar, Mumbai, Maharashtra 400099",
+          "phone": "08104312543"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84336066494598,19.024807530871726]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Star Mall, N C. Kelkar Marg, Dadar West, Dadar, Mumbai, Maharashtra 400028",
+          "phone": "08928304149"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.89194083366289,19.127177641250483]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "CTS no.73A/3, Survey no. 47, Premises Co-Operative Society Limited, Atl Corporate Park, Shop no.1, Ground Floor, 1B, Saki Vihar Rd, Powai, Mumbai, Maharashtra 400072",
+          "phone": "08104312548"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.88078284438161,19.11014738434894]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Metro Station Marol Naka, Andheri - Kurla Rd, Chimatpada, Marol, Saki Naka, Mumbai, Maharashtra 400059",
+          "phone": "08828099772"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.88301444223787,19.109498577965798]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Unit No 2 & 3 Shivai Dongre Ind Premises, Saki Naka, Mumbai, Maharashtra 400072",
+          "phone": "08928304109"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.89073920404799,19.05531427192111]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "SICH Ltd, KK Pavilion, Plot No 53, Hemu Kalani Marg, Sindhi Society, Chembur, Mumbai, Maharashtra 400071",
+          "phone": "07738098604"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.86378836839937,19.124096105758067]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Sai Elegance, Mahakali Caves Rd, Subhash Nagar, Andheri East, Mumbai, Maharashtra 400093",
+          "phone": "08928304181"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.87357306669217,19.103334790380213]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Level P10, Chhatrapati Shivaji Maharaj International Airport Mumbai, Terminal 2, Chatrapati Shivaji International Airport, Navpada, Vile Parle East, Vile Parle, Mumbai, Maharashtra 400099",
+          "phone": "07400060926"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.90201647360169,19.0728320957436]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "The Destination, Next Shopper Stop, No 001/101, Mahatma Gandhi Rd, Brindavan Colony, Chembur West, Tilak Nagar, Chembur, Mumbai, Maharashtra 400089",
+          "phone": "08928304020"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.83133853497523,19.01848391771663]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Hardcastle Restaurants Pvt Ltd,Shop No.5, Kohinoor corner Shop No.5, Kohinoor corner, Swatantryaveer Savarkar Rd, opposite Siddhi Vinayak Temple, Prabhadevi, Mumbai, Maharashtra 400025",
+          "phone": "09321710366"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.84717199162476,19.136143611193855]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Shop No 3 & 4, GF, Jaiswal chambers, SV Road Junction, Jogeshwari - Vikhroli Link Rd, Mumbai, Maharashtra 400102",
+          "phone": "08928212275"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.90984276673392,19.08944076558322]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Arihant Height, Behind Jhunjunwala collage, Laxminarayan Road, west, near Ghatkopar Railway Station, opposite side of Hotel Anand, Mumbai, Maharashtra 400086",
+          "phone": "08591303078"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82678791092032,18.931724070013132, ]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Shop No 26, Khetan Bhavan, Jamshedji Tata Road, Churchgate, Mumbai, Maharashtra 400020",
+          "phone": "09324914345"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.83451228048206,18.939047669027516]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Empire Building, No 134 to 136, Dr Dadabhai Naoroji Rd, next to Mc Donald's, opp. CST Station, Azad Maidan, Fort, Mumbai, Maharashtra 400001",
+          "phone": "08928304006"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82662626859336,18.980638010177923]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Round Building, Bellagio Tower shop no.3,6,7,8,9,14 Ground, Saat Rasta, Mahalakshmi, Mumbai, Maharashtra 400011",
+          "phone": "09321288714"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82852519539567,19.003204084324466]
+        },
+        "properties": {
+          "name": "McDonald's",
+          "address": "Trade World B, Ground Floor, Shop no. 4, 5, 10, 11, Kamala Mills Compound, Senapati Bapat Marg, Lower Parel, Mumbai, Maharashtra 400013",
+          "phone": "08356811326"
+        }
+      }
+      // {
+      //   "type": "Feature",
+      //   "geometry": {
+      //     "type": "Point",
+      //     "coordinates": [72.83182967683666,18.995190037925266]
+      //   },
+      //   "properties": {
+      //     "name": "Pizza outlet 29",
+      //     "address": "Shop no 1, Janta Restaurant, Manjunath Devashthala, NM Joshi Marg, Lower Parel, Mumbai, Maharashtra 400013",
+      //     "phone": "08828208426"
+      //   }
+      // }
+]
